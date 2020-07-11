c048ce7ea2a1948442d008c03dd5921658553854
Lucas Marinho <marinholucasit@gmail.com>
2020-07-11 18:52:19 -0300

    Creating project

---
+++ src/main/java/com/desafio/apidesafio/ApidesafioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.apidesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidesafioApplication.class, args);
+	}
+
+}
